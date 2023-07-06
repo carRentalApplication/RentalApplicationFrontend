530dabfd1d8a85fa1b2cb5f21876711063bb57e1
Ankush Raj <129718487+AnkushRaj0101@users.noreply.github.com>
2023-07-07 00:10:07 +0530

    admin side brand and vehicle (#8)
    admin side brand and vehicle

--- carRentalFrontend/src/app/admin/manage-brand/manage-brand.component.ts
+++ carRentalFrontend/src/app/admin/manage-brand/manage-brand.component.ts
@@ -1,3 +1,5 @@
+import { Brand } from 'src/app/model/Brand.model';
+import { BrandService } from './../../services/brand.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageBrandComponent implements OnInit {
 
-  constructor() { }
+  constructor(private brandService:BrandService) { }
+
+    //for pagination
+    currentPage: number = 1;
+    pageSize: number = 4;
+    totalItems: number = 0;
+    totalPages: number = 0;
+
+  public brandData: Brand[] =[]
 
   ngOnInit(): void {
+    this.getAllBrand()
   }
+
+  getAllBrand(){
+    this.brandService.getAllBrands().subscribe(res=>{
+      console.log(res);
+      this.brandData=res;
+    })
+  }
+
+
+  //for pagination methods start
+  // changePage(pageNumber: number) {
+  //   if (pageNumber >= 1 && pageNumber <= this.totalPages) {
+  //     this.currentPage = pageNumber;
+  //   }
+  // }
+
+  //  getCurrentPageProducts(): Brand[] {
+  //   this.getAllBrand();
+  //   this.totalItems = this.brandData.length;
+  //   this.totalPages = Math.ceil(this.totalItems / this.pageSize);
+  //   const startIndex = (this.currentPage - 1) * this.pageSize;
+  //   const endIndex = startIndex + this.pageSize;
+  //   return this.brandData.slice(startIndex, endIndex);
+  //  }
+  //for oagination method ends
+
 
 }
