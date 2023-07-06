48aa87f5357c74b9a25b6076d77fe1c0f6b26858
Ankush Raj <129718487+AnkushRaj0101@users.noreply.github.com>
2023-07-07 00:53:13 +0530

    Update register.component.ts

--- carRentalFrontend/src/app/register/register.component.ts
+++ carRentalFrontend/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { AuthService } from '../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { GlobalConstants } from '../shared/global-constants';
 
 @Component({
   selector: 'app-register',
